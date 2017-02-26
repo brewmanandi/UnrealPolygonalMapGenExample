3be26fa43445c648155f909dd9007870df7cb75a
Jay2645 <disneylandjay@gmail.com>
2017-02-25 19:40:40 -0800

    Initial commit

---
+++ Source/PolygonalMapExample/PolygonalMapExampleGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PolygonalMapExample.h"
+#include "PolygonalMapExampleGameModeBase.h"
+
+
+
+
---
+++ Source/PolygonalMapExample/PolygonalMapExampleGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "PolygonalMapExampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POLYGONALMAPEXAMPLE_API APolygonalMapExampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
